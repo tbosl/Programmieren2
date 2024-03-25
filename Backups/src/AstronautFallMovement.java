f74acf5cd6e71227e83089a234bf87168fcf6fac
Tobias Bosl <tboslffb@gmail.com>
2024-03-25 01:18:34 +0100

    feat: add backups. [-]

---
+++ Backups/src/AstronautFallMovement.java
@@ -0,0 +1,12 @@
+public class AstronautFallMovement {
+    // In astronaut before moveToPostion()
+    // speedInPixel = position.getY() != targetPosition.getY() ? 2 : 0.25;
+
+    // var in AstronautMovementPatterns
+    //    static final double GROUND_LEVEL = GameView.HEIGHT - 50;
+    //
+    //    First Line of nextTargetPosition
+    //    if (astronaut.getPosition().getY() < GROUND_LEVEL) {
+    //       return new Position(astronaut.getPosition().getX(), GROUND_LEVEL);
+    //    }
+}
